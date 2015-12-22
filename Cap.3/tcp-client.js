81d46d656a77a9a693670cf75d4c494e92fdad54
Jimmy_RoSa <jimmy_sidney@hotmail.es>
2015-12-22 14:26:25 -0600

    se realizo el cliente para probar el archivo tcp-server.js

---
+++ Cap.3/tcp-client.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Jimmy Rodriguez on 22/12/2015.
+ */
+
+var net = require('net');
+
+var client = new net.createSocket(); // se crea un objeto cliente de tipo nuevo socket
+client.setEncoding('utf8'); //establece el formato del conjunto de caracteres
+
+
+//conectarse al server
+client.connect('8124','localhost', function(){
+
+
+    console.log('Servidor conectado');
+    client.write('quien necesita el browser para comunicarse?');
+
+
+    //preparar el terminal para la entrada de informacion
+    process.stdin.resumen();
+
+    //cuando se reciben datos, se envian para el server
+    process.stdin.on('data',function(data){
+
+        client.write(data);
+
+    });
+
+    //cuando se reciben los datos de regreso, imprimir en la consola
+
+    client.on('data',function(data) {
+        console.log(data);
+    });
+
+
+    //cuando el server cierra conexion
+    client.on('close',function() {
+        console.log('connection is closed');
+    });
+
+
+});
+
+
--- Cap.3/tcp-server.js
+++ Cap.3/tcp-server.js
@@ -1,3 +1,30 @@
 /**
  * Created by Jimmy Rodriguez on 22/12/2015.
  */
+
+
+
+var net = require('net'); // se carga el modulo
+
+var server = net.createServer(function(conn){
+
+    console.log('conectado');
+
+    conn.on('data', function(data){
+
+       console.log(data + 'from ' + conn.remoteAddress + ' ' + conn.remotePort );
+
+        conn.write('Repitiendo: ' + data);
+
+    });
+
+    conn.on('Close', function(){
+
+        console.log('El cliente ha cerrado la conexion')
+    });
+
+
+
+}).listen(8124);
+
+console.log('escuchando por el puerto 8124');
