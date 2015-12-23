d2afc27848d9ae3a53315ac77ac0a08b331bce55
Jimmy_RoSa <jimmy_sidney@hotmail.es>
2015-12-23 12:23:21 -0600

    se creo la carpeta Cap.4 y se agrego un archivo promise.js

---
+++ Cap.5/promise.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Jimmy Rodriguez on 23/12/2015.
+ */
+
+function test_and_load (filename){//start function test_and_load
+
+    var promise =  new process.Promise();
+
+    fs.stat(filename).addCallback(function(stat){//start funcion anonima
+
+        if(!stat.isFile()){//start if
+
+            promise.emitSucess();
+
+            return;
+
+        }//end if
+
+        //otra manera de leer un archivo  dentro de
+        fs.readFile(filename).andCallback(function (data){//start function anonima
+
+            promise.emitSuccess(data);
+
+            //end function anonima
+        }).addErrback(function(error){//start function anonima
+
+            promise.emitError(error);
+
+        });//end function anonima
+
+
+    }).addErrback(function(error){
+
+            promise.emitError(error);
+
+    });//end function test_and_load
+
+}//end function test_and_load
+
+
+
+
+
+
